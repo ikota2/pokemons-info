9a640756301cbebe67663d64ccb00a9d77ca9d25
Ivan <polyzay@gmail.com>
2021-02-22 18:47:22 +0300

    Updates

---
+++ precache-manifest.2819fe2098a745ab5bd5a21dd43625fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9ba846bd93cd581f77ef44cb60f07d",
+    "url": "/pokemons-info/catalog/0/index.html"
+  },
+  {
+    "revision": "6cfb243ead149e3f3f27",
+    "url": "/pokemons-info/catalog/0/static/css/main.802d86a1.chunk.css"
+  },
+  {
+    "revision": "82b225f882121f1aa32f",
+    "url": "/pokemons-info/catalog/0/static/js/2.934ef391.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/pokemons-info/catalog/0/static/js/2.934ef391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cfb243ead149e3f3f27",
+    "url": "/pokemons-info/catalog/0/static/js/main.7eed171a.chunk.js"
+  },
+  {
+    "revision": "4f451969ee9436c25d96",
+    "url": "/pokemons-info/catalog/0/static/js/runtime-main.057aad14.js"
+  }
+]);
